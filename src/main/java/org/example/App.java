96d6826f3bd3e5c30515cbfed9b7be47d8565102
Flo <104248674+flodro34@users.noreply.github.com>
2023-05-12 15:51:05 +0200

    Part 1

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,33 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //instanciation de l'objet Random
+        Random random = new Random();
+
+        //Génération de nb aléatoire
+        int nbADeviner = random.nextInt(100) + 1;
+
+        //Instanciation de l'objet Scanner
+        Scanner sc = new Scanner(System.in);
+
+        int saisie = 0;
+        //int essai = 5;
+
+        System.out.println("Devine un nb entre 1 et 100");
+
+        do {
+            System.out.println("Entre un nb :");
+            saisie = sc.nextInt();
+
+            if (nbADeviner < saisie){
+                System.out.println("le nb est + petit ");
+            } else if (nbADeviner > saisie) {
+                System.out.println("le nb est + grand ");
+            }else {
+                System.out.println("Gagné !");
+            }
+        }while (saisie != nbADeviner);
+
+
     }
 }
